a52ed455a86d4ffa3ff0c7458d51a7ec9f4951b3
Deepsoni5 <fichadiyadeep@gmail.com>
2024-03-05 18:59:58 +0530

    stats component done

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -1,4 +1,5 @@
 import ProfileLink from "@/components/shared/ProfileLink";
+import QuestionTab from "@/components/shared/QuestionTab";
 import Stats from "@/components/shared/Stats";
 import { Button } from "@/components/ui/button";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -74,7 +75,10 @@ const Page = async ({ params, searchParams }: URLProps) => {
           </SignedIn>
         </div>
       </div>
-      <Stats />
+      <Stats
+        totalQuestions={userInfo?.totalQuestions as number}
+        totalAnswers={userInfo?.totalAnswers as number}
+      />
       <div className="mt-10 flex gap-10">
         <Tabs defaultValue="top-posts" className="flex-1">
           <TabsList className="background-light800_dark400 min-h-[42px] p-1">
@@ -85,8 +89,10 @@ const Page = async ({ params, searchParams }: URLProps) => {
               Answers
             </TabsTrigger>
           </TabsList>
-          <TabsContent value="top-posts">POSTS</TabsContent>
-          <TabsContent value="answers">ANSWERS</TabsContent>
+          <TabsContent value="top-posts">
+            <QuestionTab />
+          </TabsContent>
+          <TabsContent value="answers">AnswerTab</TabsContent>
         </Tabs>
       </div>
     </>
---
+++ components/shared/QuestionTab.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const QuestionTab = () => {
+  return <div>Question</div>;
+};
+
+export default QuestionTab;

b1d80b2537318aaab8d0d506c4a9426b9a5c792e
Deepsoni5 <fichadiyadeep@gmail.com>
2024-03-09 16:04:33 +0530

    fixed leftsidebar

--- constants/index.ts
+++ constants/index.ts
@@ -23,11 +23,6 @@ export const sidebarLinks: SidebarLink[] = [
     label: "Collections",
   },
   {
-    imgURL: "/assets/icons/suitcase.svg",
-    route: "/",
-    label: "Find Jobs",
-  },
-  {
     imgURL: "/assets/icons/tag.svg",
     route: "/tags",
     label: "Tags",
